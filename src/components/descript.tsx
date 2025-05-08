10dfe243aa8218655d5e24e948a23ed62583720e
AirInWind <aaronhuynh2004@gmail.com>
2025-05-07 19:46:26 -0700

    simplified descript.tsx

--- src/components/descript.tsx
+++ src/components/descript.tsx
@@ -1,17 +1,14 @@
 const descript = () => {
   return (
-    <div>
-      <p className="font-inter text-center text-[32px]">
+    <div className="font-inter text-center">
+      <p className="text-2xl">
         The UCR Purity Test is a way for UCR students to track their
-      </p>
-      <p className="font-inter text-center text-[32px]">
+        <br />
         experiences throughout their stay at UCR (May be judged if all actions
+        <br />
+        are completed)
       </p>
-      <p className="font-inter text-center text-[32px]">are completed)</p>
-      <br></br>
-      <p className="font-inter text-center text-[23px]">
-        Click on all items you have done!
-      </p>
+      <p className="pt-5 text-lg">Click on all items you have done!</p>
     </div>
   );
 };
