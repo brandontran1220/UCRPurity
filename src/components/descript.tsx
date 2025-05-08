5bb7df5cbef38d9a1f8914e7bfed7cc7c1684361
AirInWind <aaronhuynh2004@gmail.com>
2025-05-07 17:36:39 -0700

    readjusted text

--- src/components/descript.tsx
+++ src/components/descript.tsx
@@ -1,17 +1,22 @@
 const descript = () => {
-  return (
-    <div>
-      <p className="font-inter text-center text-[32px]">
-        The UCR Purity Test is a way for UCR students to track their experiences
-        throughout their stay at UCR (May be judged if all actions are
-        completed)
-      </p>
-      <br></br>
-      <p className="font-inter text-center text-[23px]">
-        Click on all items you have done!
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <p className="font-inter text-[32px] text-center">
+            The UCR Purity Test is a way for UCR students to track their 
+            </p>
+            <p className="font-inter text-[32px] text-center">
+            experiences throughout their stay at UCR (May be judged if all actions 
+            </p>
+            <p className="font-inter text-[32px] text-center">
+            are completed)
+            </p>
+            <br>
+            </br>
+            <p className="font-inter text-[23px] text-center">
+                Click on all items you have done!
+            </p>
+        </div>
+    );
 };
 
-export default descript;
+export default descript
