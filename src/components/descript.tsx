042d040d08c2c74f20aa72e816e71d9152c59c09
AirInWind <aaronhuynh2004@gmail.com>
2025-05-07 17:21:38 -0700

    added center text description

---
+++ src/components/descript.tsx
@@ -0,0 +1,17 @@
+const descript = () => {
+  return (
+    <div>
+      <p className="font-inter text-center text-[32px]">
+        The UCR Purity Test is a way for UCR students to track their experiences
+        throughout their stay at UCR (May be judged if all actions are
+        completed)
+      </p>
+      <br></br>
+      <p className="font-inter text-center text-[23px]">
+        Click on all items you have done!
+      </p>
+    </div>
+  );
+};
+
+export default descript;
