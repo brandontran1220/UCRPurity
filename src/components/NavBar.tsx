1925b4847db820abc19cb9a990163c7c62731460
Tony Zhou <tzhou070@ucr.edu>
2025-05-13 17:10:52 -0300

    active page for nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,37 +1,43 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 
 const NavBar = () => {
-  return (
+  const [currentPath, setCurrentPath] = useState("");
 
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+
+  const navItems = [
+    { label: "Home", href: "/" },
+    { label: "Statistics", href: "/stats" },
+    { label: "Team", href: "/board" },
+  ];
+
+  return (
     <nav className="flex w-full items-center justify-center gap-[430px] mt-5">
       {/* Left side: UCRPurity as a link */}
-      <a href="/" className="font-Inter text-lg font-bold text-gray-800">
+      <a href="/" className="font-Inter text-lg font-bold text-purity-black-100">
         <span className="text-blue-600">UCR</span>Purity
       </a>
 
       {/* Right side: Nav links */}
-      <ul className="font-Inter relative flex text-gray-800 space-x-9">
-        <li>
-          <a href="/" className="border-b-2 border-blue-600 text-blue-600">
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="/stats"
-            className="border-b-2 border-transparent hover:border-blue-200 hover:text-blue-200"
-          >
-            Statistics
-          </a>
-        </li>
-        <li>
-          <a
-            href="/board"
-            className="border-b-2 border-transparent hover:border-blue-200 hover:text-blue-200"
-          >
-            Team
-          </a>
-        </li>
+      <ul className="font-Inter relative flex text-purity-black-100 space-x-9">
+        {navItems.map((item) => (
+          <li key={item.href}>
+            <a
+              href={item.href}
+              className={`border-b-2 transition-all duration-200 ${
+                currentPath === item.href
+                  ? "border-purity-blue-200 text-purity-blue-200"
+                  : "border-transparent hover:border-blue-200 hover:text-blue-200"
+              }`}
+            >
+              {item.label}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
