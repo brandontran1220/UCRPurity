e44d310e4cad59b591f4ad8164899e8854570a7a
Zachary Nobunaga <znobunaga11@gmail.com>
2025-05-23 13:45:02 -0700

    fixed Prettier formatting issues

--- src/components/ErrorButton.tsx
+++ src/components/ErrorButton.tsx
@@ -8,7 +8,7 @@ const ErrorButton = () => {
     <div className="mt-6 flex justify-center">
       <button
         onClick={() => router.push("/")}
-        className="rounded-xl bg-blue-600 px-5 py-3 text-white font-semibold shadow-md transition hover:bg-blue-700"
+        className="rounded-xl bg-blue-600 px-5 py-3 font-semibold text-white shadow-md transition hover:bg-blue-700"
       >
         Go Back Home
       </button>
