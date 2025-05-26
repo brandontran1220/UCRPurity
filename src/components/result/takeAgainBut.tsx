827d38f5a307ad4232f6b6953e8a92af06f85232
Brandon Tran <brandontran1220@gmail.com>
2025-05-25 23:37:31 -0700

    Add screenshot button on results page

--- src/components/result/takeAgainBut.tsx
+++ src/components/result/takeAgainBut.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const TakeAgainBut = () => {
   return (
-    <div className="py- my-10 flex justify-center">
+    <div className="flex justify-center">
       <Link
         href="/"
         className="rounded-2xl bg-blue-600 px-6 py-3 text-lg font-semibold text-white shadow-md transition hover:bg-blue-700"
