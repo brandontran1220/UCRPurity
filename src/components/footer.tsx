4a0a58ac54483138eff788afe4eae5796b8f55d0
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-05-27 20:31:32 -0700

    made all footer text mobile responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Rice from "@/public/happyRice.svg";
 import { SiLinktree } from "react-icons/si";
-
+import { FaLinkedin } from "react-icons/fa"
 const quotes = [
   "Live your life to the fullest and enjoy every moment!",
   "Life is a journey, not a destination.",
@@ -21,27 +21,33 @@ const quotes = [
 
 const footer = () => {
   return (
-    <div className="bg-purity-blue-200 font-inter flex w-full items-center justify-between py-1 text-black">
+    <div className="bg-purity-blue-200 font-inter flex md:flex-row flex-col w-full items-center justify-between py-1 text-black">
       <div className="pr-30">
-        <p className="pl-5">Built By</p>
-        <p className="pl-17 font-black">Purified Rice</p>
+        <p className="pl-5 hidden md:block">Built By</p>
+        <p className="pl-17 font-black hidden md:block">Purified Rice</p>
         <Link href="/">
           <Image src={Rice} alt="Happy Rice" width={100} height={100} />
         </Link>
+        <div className="md:hidden flex items-center ">
+        <p className="font-black">Purified Rice </p>
+        <Link href="https://linktr.ee/PurifiedRice_Members" className="text-3xl pl-2"> 
+          <FaLinkedin />
+        </Link>
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center text-center">
+      <div className="hidden md:block flex flex-col items-center justify-center text-center">
         <p>{quotes[Math.floor(Math.random() * quotes.length)]}</p>
       </div>
-      <div className="pr-5 text-right">
+      <div className="hidden md:block pr-5 text-right">
         <Link
           href="https://linktr.ee/PurifiedRice_Members"
-          className="mb-18 flex items-center justify-end gap-2 font-semibold hover:text-white hover:underline"
+          className="hidden md:block mb-18 flex items-center justify-end gap-2 font-semibold hover:text-white hover:underline"
         >
           Follow Us On LinkedIn!
           <SiLinktree />
         </Link>
-        <p>Created in May 2025</p>
-        <p>@ University of California-Riverside</p>
+        <p className="hidden md:block">Created in May 2025</p>
+        <p className="hidden md:block">@ University of California-Riverside</p>
       </div>
     </div>
   );
