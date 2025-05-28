75e8b3dc33a79cbf2f7d4f03e4304b76993a730b
Tony Zhou <tzhou070@ucr.edu>
2025-05-28 01:20:06 -0300

    title banner and description mobile responsive

--- src/components/home/descript.tsx
+++ src/components/home/descript.tsx
@@ -1,12 +1,12 @@
 const descript = () => {
   return (
     <div className="font-inter pt-15 pb-5 text-center">
-      <p className="text-base translate-y-[-1.4rem] font-medium md:text-2xl md:translate-y-[0rem]">
+      <p className="translate-y-[-1.4rem] text-base font-medium md:translate-y-[0rem] md:text-2xl">
         The UCR Purity Test is a way for UCR students to
         <br />
         track their experiences through their stay at UCR!
         <br />
-      (May be judged if all actions are completed)
+        (May be judged if all actions are completed)
       </p>
     </div>
   );
