353c5afa60edb634b6933718fbf949d95a21b8e9
dillonhoh <67936878+dillonhoh@users.noreply.github.com>
2025-05-27 21:14:50 -0700

    corrected centering for mobile footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import Rice from "@/public/happyRice.svg";
-import { SiLinktree } from "react-icons/si";
-import { FaLinkedin } from "react-icons/fa"
+import { FaLinkedin } from "react-icons/fa";
+
 const quotes = [
   "Live your life to the fullest and enjoy every moment!",
   "Life is a journey, not a destination.",
@@ -21,36 +21,39 @@ const quotes = [
 
 const footer = () => {
   return (
-    <div className="bg-purity-blue-200 font-inter flex md:flex-row flex-col w-full items-center justify-between py-1 text-black">
-      <div className="pr-30">
-        <p className="pl-5 hidden md:block">Built By</p>
-        <p className="pl-17 font-black hidden md:block">Purified Rice</p>
-        <Link href="/">
-          <Image src={Rice} alt="Happy Rice" width={100} height={100} />
+    <div className="bg-purity-blue-200 font-inter grid w-full grid-cols-1 items-center justify-between py-1 text-black md:grid-cols-3">
+      <div className="flex flex-col items-center justify-center md:items-start md:pr-30">
+        <p className="hidden pl-5 md:block">Built By</p>
+        <p className="hidden pl-17 font-black md:block">Purified Rice</p>
+        <Link href="/" className="md:justify-end">
+          <Image
+            src={Rice}
+            alt="Happy Rice"
+            className="h-[70px] w-[70px] md:h-[100px] md:w-[100px]"
+          />
         </Link>
-        <div className="md:hidden flex items-center ">
-        <p className="font-black">Purified Rice </p>
-        <Link href="https://linktr.ee/PurifiedRice_Members" className="text-3xl pl-2"> 
-          <FaLinkedin />
-        </Link>
-        </div>
       </div>
-      <div className="hidden md:block flex flex-col items-center justify-center text-center">
-        <p>{quotes[Math.floor(Math.random() * quotes.length)]}</p>
+      <div className="flex flex-col items-center justify-center text-center md:my-0">
+        <p className="hidden md:block">
+          {quotes[Math.floor(Math.random() * quotes.length)]}
+        </p>
       </div>
-      <div className="hidden md:block pr-5 text-right">
+      <div className="flex flex-col items-center justify-center text-center md:items-end md:pr-5 md:text-right">
         <Link
           href="https://linktr.ee/PurifiedRice_Members"
-          className="hidden md:block mb-18 flex items-center justify-end gap-2 font-semibold hover:text-white hover:underline"
-        >
-          Follow Us On LinkedIn!
-          <SiLinktree />
+          className="mt-3 mb-3 flex items-center justify-end gap-2 font-semibold hover:text-white hover:underline md:mb-23">
+          <p className="hidden md:block">Follow Us On LinkedIn!</p>
+          <p className="md:hidden">Purified Rice</p>
+          <p className="text-3xl md:text-xl">
+            <FaLinkedin />
+          </p>
         </Link>
-        <p className="hidden md:block">Created in May 2025</p>
-        <p className="hidden md:block">@ University of California-Riverside</p>
-      </div>
+          <p className="hidden md:block">@ University of California-Riverside</p>
+          {/* <p className="md:hidden pl-5 text-2xl md:text-xl">
+              <FaLinkedin />
+          </p> */}
+        </div>
     </div>
   );
 };
-
 export default footer;
