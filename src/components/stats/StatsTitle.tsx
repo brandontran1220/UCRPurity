88a8e30fa2e7bac921a8996ed694a5d66d176c73
Brandon Tran <brandontran1220@gmail.com>
2025-05-30 15:00:59 -0700

    Add analytics and text style

--- src/components/stats/StatsTitle.tsx
+++ src/components/stats/StatsTitle.tsx
@@ -1,7 +1,12 @@
 const StatsTitle = () => {
   return (
-    <div className="jusitfy-center font-inter m-6 mx-25 flex flex-col items-center text-center text-lg font-black md:m-12 md:text-3xl">
-      <p>Rice Purity Test Community Statistics</p>
+    <div className="font-inter m-6 mx-25 flex flex-col items-center justify-center text-center text-lg font-black md:m-12 md:text-3xl">
+      <p className="text-purity-blue-200">
+        UCR
+        <span className="text-3xl font-black text-black">
+          Purity Test Community Statistics
+        </span>
+      </p>
     </div>
   );
 };
